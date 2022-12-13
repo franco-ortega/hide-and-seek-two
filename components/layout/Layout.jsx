7ff7db8f88db277f1ff948e21248b3857acaec04
Franco Ortega <francopdx@gmail.com>
2022-12-13 11:43:50 -0800

    provide initial dev styles to Layout

--- components/layout/Layout.jsx
+++ components/layout/Layout.jsx
@@ -1,16 +1,17 @@
 import Head from 'next/head';
 import Header from '../header/Header';
+import styles from './Layout.module.scss';
 
 const Layout = ({ children }) => {
   return (
-    <div data-testid='layout'>
+    <div data-testid='layout' className={styles.Layout}>
       <Head>
         <title>Hide and Seek Game</title>
         <meta name='description' content='Hide and Seek game' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <Header />
-      <main>{children}</main>;
+      <main>{children}</main>
     </div>
   );
 };
