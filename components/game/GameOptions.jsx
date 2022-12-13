15fc17e87e4cb5ac7dd2cb0dbc61b80af9d19754
Franco Ortega <francopdx@gmail.com>
2022-12-13 15:41:52 -0800

    fill in human player names

--- components/game/GameOptions.jsx
+++ components/game/GameOptions.jsx
@@ -33,47 +33,49 @@ const GameOptions = () => {
   console.log(namePlayers);
 
   return (
-    <form
-      data-testid='game-options'
-      className={styles.GameOptions}
-      onSubmit={onPlayGameSubmit}
-    >
-      <h2>Game Options</h2>
-      <p>Select the number of Players:</p>
-      <label htmlFor='human-players'>
-        Humans
-        <input
-          type='number'
-          id='human-players'
-          name='humans'
-          defaultValue={0}
-        />
-      </label>
-      <label htmlFor='bot-players'>
-        Bots
-        <input type='number' id='bot-players' name='bots' defaultValue={0} />
-      </label>
-      <label htmlFor='difficulty'>
-        <p>Select the Difficulty:</p>
-        <select name='difficulty' id='difficulty'>
-          <option value=''></option>
-          <option value='easy'>Easy</option>
-          <option value='medium'>Medium</option>
-          <option value='hard'>Hard</option>
-        </select>
-      </label>
-      <label htmlFor='duration'>
-        <p>Select the Duration:</p>
-        <select name='duration' id='duration'>
-          <option value=''></option>
-          <option value='3'>3 rounds</option>
-          <option value='5'>5 rounds</option>
-          <option value='7'>7 rounds</option>
-        </select>
-      </label>
-      <button>Proceed</button>
-      {namePlayers && <NamePlayers />}
-    </form>
+    <>
+      <form
+        data-testid='game-options'
+        className={styles.GameOptions}
+        onSubmit={onPlayGameSubmit}
+      >
+        <h2>Game Options</h2>
+        <p>Select the number of Players:</p>
+        <label htmlFor='human-players'>
+          Humans
+          <input
+            type='number'
+            id='human-players'
+            name='humans'
+            defaultValue={0}
+          />
+        </label>
+        <label htmlFor='bot-players'>
+          Bots
+          <input type='number' id='bot-players' name='bots' defaultValue={0} />
+        </label>
+        <label htmlFor='difficulty'>
+          <p>Select the Difficulty:</p>
+          <select name='difficulty' id='difficulty'>
+            <option value=''></option>
+            <option value='easy'>Easy</option>
+            <option value='medium'>Medium</option>
+            <option value='hard'>Hard</option>
+          </select>
+        </label>
+        <label htmlFor='duration'>
+          <p>Select the Duration:</p>
+          <select name='duration' id='duration'>
+            <option value=''></option>
+            <option value='3'>3 rounds</option>
+            <option value='5'>5 rounds</option>
+            <option value='7'>7 rounds</option>
+          </select>
+        </label>
+        <button>Proceed</button>
+      </form>
+      {namePlayers && <NamePlayers setNamePlayers={setNamePlayers} />}
+    </>
   );
 };
 
--- components/name/NamePlayers.jsx
+++ components/name/NamePlayers.jsx
@@ -1,7 +1,7 @@
 import { useGameContext } from '../../state/GameContext';
 import styles from './NamePlayers.module.scss';
 
-const NamePlayers = () => {
+const NamePlayers = ({ setNamePlayers }) => {
   const { players, setPlayers } = useGameContext();
   // create text input for each human player to enter their name
   const humanPlayers = players.filter((player) => player.human);
@@ -10,12 +10,45 @@ const NamePlayers = () => {
   // update players state
   // proceed to game page
 
+  const onNamesSubmit = (e) => {
+    e.preventDefault();
+    console.log('Names submitted!');
+    console.log(e.target.player3.value);
+
+    setNamePlayers(false);
+
+    setPlayers((prevState) => {
+      return prevState.map((state) => {
+        const input = `player${state.playerOrder}`;
+        console.log('STATE', state);
+        console.log('PLAYER', input);
+
+        if (state.human) {
+          console.log('VALUE', e.target[input].value);
+          state.name = e.target[input].value;
+        }
+
+        return state;
+      });
+    });
+  };
+
   return (
     <div data-testid='name-players' className={styles.NamePlayers}>
-      <h3>Select Names for Human Players</h3>
-      {humanPlayers.map((human) => (
-        <input key={human.playerOrder} type='text' placeholder='Name' />
-      ))}
+      <h3>Enter Names for Human Players</h3>
+      <form onSubmit={onNamesSubmit}>
+        {humanPlayers.map((human, i) => (
+          <label key={human.playerOrder} htmlFor={`player-${i + 1}`}>
+            <input
+              type='text'
+              placeholder={`Player ${i + 1}`}
+              id={`player-${i + 1}`}
+              name={`player${human.playerOrder}`}
+            />
+          </label>
+        ))}
+        <button>Submit</button>
+      </form>
     </div>
   );
 };
