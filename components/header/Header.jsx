3e26e5353aa9229b51decbd8c22e45ebfbedefe8
Franco Ortega <francopdx@gmail.com>
2022-12-13 11:41:10 -0800

    provide initial dev styles to Header

--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -1,6 +1,8 @@
+import styles from './Header.module.scss';
+
 const Header = () => {
   return (
-    <header data-testid='header'>
+    <header data-testid='header' className={styles.Header}>
       <h1>Hide and Seek</h1>
     </header>
   );
