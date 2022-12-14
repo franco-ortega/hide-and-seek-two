60ce570020d7d21ae373bacda3d233f02fa57995
Franco Ortega <francopdx@gmail.com>
2022-12-13 16:06:55 -0800

    add human player and bot player names

--- components/name/NamePlayers.jsx
+++ components/name/NamePlayers.jsx
@@ -1,52 +1,38 @@
 import { useGameContext } from '../../state/GameContext';
+import addPlayerNames from '../../utils/addPlayerNames';
 import styles from './NamePlayers.module.scss';
 
 const NamePlayers = ({ setNamePlayers }) => {
   const { players, setPlayers } = useGameContext();
+
   // create text input for each human player to enter their name
   const humanPlayers = players.filter((player) => player.human);
-  console.log({ humanPlayers });
+  const humanNames = humanPlayers.map((human, i) => (
+    <label key={human.playerOrder} htmlFor={`player-${i + 1}`}>
+      <input
+        type='text'
+        placeholder={`Player ${i + 1}`}
+        id={`player-${i + 1}`}
+        name={`player${human.playerOrder}`}
+      />
+    </label>
+  ));
+
   // auto generate name for each bot player
   // update players state
-  // proceed to game page
-
   const onNamesSubmit = (e) => {
     e.preventDefault();
-    console.log('Names submitted!');
-    console.log(e.target.player3.value);
-
+    setPlayers((prevState) => addPlayerNames(prevState, e));
     setNamePlayers(false);
 
-    setPlayers((prevState) => {
-      return prevState.map((state) => {
-        const input = `player${state.playerOrder}`;
-        console.log('STATE', state);
-        console.log('PLAYER', input);
-
-        if (state.human) {
-          console.log('VALUE', e.target[input].value);
-          state.name = e.target[input].value;
-        }
-
-        return state;
-      });
-    });
+    // proceed to game page
   };
 
   return (
     <div data-testid='name-players' className={styles.NamePlayers}>
       <h3>Enter Names for Human Players</h3>
       <form onSubmit={onNamesSubmit}>
-        {humanPlayers.map((human, i) => (
-          <label key={human.playerOrder} htmlFor={`player-${i + 1}`}>
-            <input
-              type='text'
-              placeholder={`Player ${i + 1}`}
-              id={`player-${i + 1}`}
-              name={`player${human.playerOrder}`}
-            />
-          </label>
-        ))}
+        {humanNames}
         <button>Submit</button>
       </form>
     </div>
