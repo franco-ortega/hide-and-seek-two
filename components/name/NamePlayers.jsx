9f6320ee6268d1cd6bdf281101832c630a1921be
Franco Ortega <francopdx@gmail.com>
2022-12-13 16:15:08 -0800

    wrap provider around NamePlayers test

--- components/name/NamePlayers.jsx
+++ components/name/NamePlayers.jsx
@@ -6,20 +6,20 @@ const NamePlayers = ({ setNamePlayers }) => {
   const { players, setPlayers } = useGameContext();
 
   // create text input for each human player to enter their name
-  const humanPlayers = players.filter((player) => player.human);
-  const humanNames = humanPlayers.map((human, i) => (
-    <label key={human.playerOrder} htmlFor={`player-${i + 1}`}>
-      <input
-        type='text'
-        placeholder={`Player ${i + 1}`}
-        id={`player-${i + 1}`}
-        name={`player${human.playerOrder}`}
-      />
-    </label>
-  ));
+  const humanPlayers = players
+    .filter((player) => player.human)
+    .map((human, i) => (
+      <label key={human.playerOrder} htmlFor={`player-${i + 1}`}>
+        <input
+          type='text'
+          placeholder={`Player ${i + 1}`}
+          id={`player-${i + 1}`}
+          name={`player${human.playerOrder}`}
+        />
+      </label>
+    ));
 
-  // auto generate name for each bot player
-  // update players state
+  // add player names and create bot names
   const onNamesSubmit = (e) => {
     e.preventDefault();
     setPlayers((prevState) => addPlayerNames(prevState, e));
@@ -32,7 +32,7 @@ const NamePlayers = ({ setNamePlayers }) => {
     <div data-testid='name-players' className={styles.NamePlayers}>
       <h3>Enter Names for Human Players</h3>
       <form onSubmit={onNamesSubmit}>
-        {humanNames}
+        {humanPlayers}
         <button>Submit</button>
       </form>
     </div>
--- components/name/NamePlayers.test.jsx
+++ components/name/NamePlayers.test.jsx
@@ -1,9 +1,14 @@
 import { render, screen } from '@testing-library/react';
+import { GameProvider } from '../../state/GameContext';
 import NamePlayers from './NamePlayers';
 
 describe('NamePlayers component tests', () => {
   it('renders NamePlayers component', () => {
-    render(<NamePlayers />);
+    render(
+      <GameProvider>
+        <NamePlayers />
+      </GameProvider>
+    );
 
     const element = screen.getByTestId('name-players');
 
