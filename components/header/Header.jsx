871fe8ea7708720b077c11b58bf5a49eeb370a10
Franco Ortega <francopdx@gmail.com>
2022-12-15 15:30:58 -0800

    create temporary filler nav

--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -4,6 +4,13 @@ const Header = () => {
   return (
     <header data-testid='header' className={styles.Header}>
       <h1>Hide and Seek</h1>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Game</li>
+          <li>Results</li>
+        </ul>
+      </nav>
     </header>
   );
 };
