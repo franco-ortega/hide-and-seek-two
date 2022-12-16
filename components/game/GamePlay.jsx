3a74509b0c32f150d7a5315f9ebacc15bf413a61
Franco Ortega <francopdx@gmail.com>
2022-12-15 16:03:14 -0800

    display difficulty and duration

--- components/game/GamePlay.jsx
+++ components/game/GamePlay.jsx
@@ -2,10 +2,12 @@ import { useGameContext } from '../../state/GameContext';
 import styles from './GamePlay.module.scss';
 
 const GamePlay = () => {
-  const { players } = useGameContext();
+  const { players, difficulty, duration } = useGameContext();
 
   return (
     <div data-testid='game-play' className={styles.GamePlay}>
+      <div>Difficulty: {difficulty}</div>
+      <div>Duration: {duration} rounds</div>
       <ul>
         {players.map((player) => (
           <li key={player.playerOrder}>
