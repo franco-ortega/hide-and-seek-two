bd01a6b4a70bf3abea5ec5c78fc63c02fe06464a
Franco Ortega <francopdx@gmail.com>
2022-12-15 16:22:47 -0800

    create Game component

---
+++ components/game/Game.jsx
@@ -0,0 +1,24 @@
+import styles from './Game.module.scss';
+
+const Game = () => {
+  return (
+    <div className={styles.Game} data-testid='game'>
+      <h3>Game Field</h3>
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam,
+        possimus doloribus modi aliquid minima ipsum temporibus sit numquam
+        debitis optio officiis adipisci voluptates a, dolorum consequatur eos
+        neque eum illo alias vero quaerat delectus quasi itaque? Numquam sint
+        repellat quae. Sunt nemo animi deleniti iste expedita praesentium
+        consequuntur repellat alias necessitatibus inventore esse dignissimos
+        quam assumenda perferendis ipsum, excepturi voluptate minus ad nulla
+        nihil. Sequi consequatur at esse facere soluta cupiditate, magnam
+        voluptatem. Odit nihil blanditiis fugit vel deserunt. Mollitia illo
+        itaque accusamus eligendi aut quas amet, perspiciatis distinctio, autem
+        a tempore in, inventore incidunt recusandae. Nisi minima enim commodi.
+      </div>
+    </div>
+  );
+};
+
+export default Game;
--- components/game/GamePlay.jsx
+++ components/game/GamePlay.jsx
@@ -1,9 +1,11 @@
 import Scoreboard from '../scoreboard/Scoreboard';
+import Game from './Game';
 import styles from './GamePlay.module.scss';
 
 const GamePlay = () => {
   return (
     <div data-testid='game-play' className={styles.GamePlay}>
+      <Game />
       <Scoreboard />
     </div>
   );
